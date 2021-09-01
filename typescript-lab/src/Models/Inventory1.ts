afa40e10cae541fc0216e3a6c81c82cbb5cd154c
djtoler <84260551+djtoler@users.noreply.github.com>
2021-09-01 08:57:41 -0400

    fnal

---
+++ typescript-lab/src/Models/Inventory1.ts
@@ -0,0 +1,7 @@
+export default interface InventoryItem{
+
+
+
+    product: string,
+    quantity: number,
+}
---
+++ typescript-lab/src/Models/Mountain.ts
@@ -0,0 +1,8 @@
+export default interface Mountain{
+
+
+
+    name: string,
+    height: number,
+}
+
